2e330342172a4a2fd60aef108e01434b2e9b65ea
Julio-paz961 <64233534+Julio-paz961@users.noreply.github.com>
2021-02-04 16:23:47 -0300

    Update _Hero-style.tsx

--- my-app/components/Hero/_Hero-style.tsx
+++ my-app/components/Hero/_Hero-style.tsx
@@ -4,7 +4,7 @@ export const Space=styled.div`
     padding:40px;
 
 `
-export const HeroStyle=styled.h1`
+export const HeroText=styled.h1`
     font-size:2.2em;
     margin:0;
     font-family:Inter, sans-serif;
