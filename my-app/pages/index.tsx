b86456000ab0e10688e5847e69c850184e403142
Julio-paz961 <64233534+Julio-paz961@users.noreply.github.com>
2021-02-04 22:03:48 -0300

    Update index.tsx

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
-        <Background className="backgroundHero"> 
+        <Background> 
           <Menu/>
           <Hero/>
         </Background>
